c06b3025160578d2b50377873e708e65a3f440d1
TheRealEzCoins <stevens.ian633@gmail.com>
2024-01-22 22:14:36 +0100

    Refactor item creation into a separate component and update App.vue
    The main component, ItemCreationPage, has been introduced, moving all logic related to item creation to this new component. The App.vue file was updated to reference this component accordingly. Additionally, the existing empty ItemTemplate.vue file has been removed as part of this refactoring process.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,45 @@
 <template>
   <div id="app">
-    <div class="item-creation-pages">
-      <ItemCreationPage v-for="n in 9" :key="n" :collapsed="true" :selected_small_number="n" @click="toggleCollapse(n)" />
+    <div style="position: sticky; top: 0;">
+      <details>
+        <summary>Tag Creation</summary>
+        <div class="item-creation-pages">
+          <div style="display:flex; flex-direction:column;">
+            <input type="text" placeholder="Tag name" v-model="tagName">
+
+            <div>
+              <input type="radio" id="radio1" v-model="selectedRadio" value="3DItem">
+              <label for="radio1">3D Item</label>
+              <input type="radio" id="radio2" v-model="selectedRadio" value="Tag">
+              <label for="radio2">Tag</label>
+              <input type="text" placeholder="Inventory Slot" v-model="inventorySlot">
+              <input type="text" v-if="selectedRadio === '3DItem'" placeholder="Custom Model Data" id="custom-model-data">
+            </div>
+            <input type="text" placeholder="Emoji" maxLength="2" id="tag-emoji">
+          </div>
+        </div>
+      </details>
+      <details>
+        <summary>Item Creation</summary>
+        <div class="item-creation-pages">
+          <ItemCreationPage v-for="n in 9" :key="n" :collapsed="true"
+                            :selected_small_number="n"
+                            @click="toggleCollapse(n)"
+                            ref="itemRefs" /> <!-- Add ref here -->
+        </div>
+      </details>
+      <button @click="generateItems">Generate</button>
+
+      <div v-if="showPopup" class="popup">
+        <h3>Preview</h3>
+        <pre style="overflow-y:auto; height:100px;">{{ previewData }}</pre>
+        <a :href="downloadUrl" download="data.txt">Download</a>
+        <button @click="closePopup">Close</button>
+      </div>
     </div>
-    <button @click="generateItems">Generate</button>
   </div>
 </template>
+
 <script>
 import ItemCreationPage from './components/pages/ItemCreationPage.vue';
 
@@ -16,15 +50,68 @@ export default {
   data() {
     return {
       items: Array.from({length: 9}, () => true),
+      selectedRadio : null,
+      tagName: '',
+      inventorySlot: '',
+      customModelData: '',
+      emoji: '',
+      showPopup: false,
+      downloadUrl: null,
+      allItems: [],
+    }
+  },
+  computed: {
+    previewData() {
+      return`${this.tagName}:\n
+             name: '${this.tagName}'
+             material: ${this.getMaterial(this.selectedRadio)}
+             slot: ${this.inventorySlot}
+             ${this.getType(this.selectedRadio)}
+             ${this.allItems.map(item => this.returnAsText(item))}
+          `; // Modifed here
     }
   },
   methods: {
+    getMaterial(selectedRadio) {
+      if(selectedRadio === '3D radio') {
+        return 'FEATHER'
+      } else {
+        return 'NAME_TAG'
+      }
+    },
+    getType(selectedRadio) {
+      if(selectedRadio === '3D radio') {
+        return `custom_model_data: ${this.customModelData}\n\
+        command: o give {PLAYER} ${this.tagName} 1`
+      } else {
+        return `command: lp user {PLAYER} permission set deluxetags.tag.${this.tagName} server=towny`
+      }
+    },
+
     toggleCollapse(index) {
       this.items[index - 1] = !this.items[index - 1];
     },
+    returnAsText(item) {
+      const lore = item.lore.join('. ');
+      return `
+  '${item.item_number}'\n\
+    material: ${item.material}\n\
+    name: '${item.name}'\n\
+    lore:\n\
+    - '&8${lore.split('. ').join("\n    - '&8")}'\n\
+    - '&8      '\n\
+    - '&e${item.selected_month} ${new Date().getFullYear()} Collectors Item'`
+    },
+
     generateItems() {
-      // Add generation logic here
-    }
+      this.allItems = this.$refs.itemRefs.map((ref) => ref.item); // Modify this to get Item from each ItemCreationPage
+      const blob = new Blob([this.previewData], { type: 'text/yml' });
+      this.downloadUrl = URL.createObjectURL(blob);
+      this.showPopup = true;
+    },
+    closePopup() {
+      this.showPopup = false;
+    },
   }
 };
 </script>
@@ -34,9 +121,25 @@ export default {
   height: 100vh;
   display: flex;
   flex-direction: column;
+  align-items: center;
 }
+
+.popup {
+  position: fixed;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  background: dimgray;
+  padding: 20px;
+  border: 1px solid black;
+  border-radius: 10px; /* Added this for a more professional look */
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.15); /* Added this for a more professional look */
+}
+
 .item-creation-pages {
   display: flex;
   flex-wrap: wrap;
+  justify-content: center;
 }
+
 </style>
--- src/components/pages/ItemCreationPage.vue
+++ src/components/pages/ItemCreationPage.vue
@@ -1,104 +1,38 @@
-<script>
-import { ref } from 'vue';
-import Items from '../../assets/items.json';
-
-export default {
-  props: {
-    selected_small_number: {
-      type: Number,
-      required: true
-    }
-  },
-  data() {
-    return {
-      Items,
-      selected_month: null,
-      selected_option: null,
-      selected_large_number: null,
-      name: "",
-      lores: [""],
-      textBox1: "",
-      textBox2: "",
-      current_year: new Date().getFullYear(),
-      monthOptions: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
-      open: false
-    };
-  },
-  computed: {
-    itemIDs() {
-      return this.Items.map(item => item.name).sort();
-    },
-  },
-  methods: {
-    formatItemName(name) {
-      return name.split('_')
-          .map(word => word.charAt(0).toUpperCase() + word.slice(1))
-          .join(' ');
-    },
-    getIcon(name) {
-      return `./src/assets/items/${name}.png`;
-    },
-    addLoreField() {
-      this.lores.push("");
-    },
-    fillTextBoxes(){
-      const lore = this.lores.join('. ');
-
-      this.textBox1 = `
-  '${this.selected_small_number}'\n\
-    material: ${this.selected_option.toUpperCase()}\n\
-    name: '${this.name}'\n\
-    lore:\n\
-    - '&8${lore.split('. ').join("\n    - '&8")}'\n\
-    - '&8      '\n\
-    - '&e${this.selected_month} ${this.current_year} Collectors Item'`;
-
-      this.textBox2 = JSON.stringify({
-        name: this.name,
-        lore: lore
-      }, null, 2);
-    },
-    toggleOpen() {
-      this.open = !this.open;
-    }
-  },
-};
-</script>
 <template>
   <div class="item-creator">
     <div class="top-bar" @click="toggleOpen">
       <p class="centered-text">
         <span class="bordered-text">
-          {{open ? `Collapse ${selected_small_number}` : `Expand ${selected_small_number}`}}
+          {{open ? `▼ ${selected_small_number}` : `▶ ${selected_small_number}`}}
         </span>
       </p>    </div>
     <div class="flex-container">
       <div class="content" v-show="open">
         <div class="align-center">
           <div class="item-detail">
-            <img v-if="selected_option" :src="getIcon(selected_option)" alt="?" class="item-icon">
-            <select v-model="selected_option" class="item-select">
+            <img v-if="item.material" :src="getIcon(item.material)" alt="?" class="item-icon">
+            <select v-model="item.material" class="item-select">
               <option v-for="name in itemIDs" :key="name" :value="name">
                 {{ formatItemName(name) }}
               </option>
             </select>
           </div>
           <div class="input-field">
-            <select v-model="selected_month" class="item-select">
+            <select v-model="item.selected_month" class="item-select">
               <option v-for="month in monthOptions" :key="month" :value="month">
                 {{ month }}
               </option>
             </select>
           </div>
           <div class="input-field">
-            <input type="number" v-model="selected_large_number" placeholder="Crate Number" class="item-input-number">
+            <input type="number" v-model="item.selected_large_number" placeholder="Crate Number" class="item-input-number">
           </div>
           <div class="input-field">
-            <input v-model="name" placeholder="Name" class="item-name">
+            <input v-model="item.name" placeholder="Name" class="item-name">
           </div>
-          <div v-for="(lore, index) in lores" :key="index" class="textarea-field inline-field">
-            <textarea v-model="lores[index]" placeholder="Lore" class="item-lore"></textarea>
-            <button v-if="index === lores.length - 1" class="add-button" @click="addLoreField">+</button>
+          <div v-for="(lore, index) in item.lore" :key="index" class="textarea-field inline-field">
+            <textarea v-model="item.lore[index]" placeholder="Lore" class="item-lore"></textarea>
+            <button v-if="index === item.lore.length - 1" class="add-button" @click="addLoreField">+</button>
           </div>
           <button class="generate-button" @click="fillTextBoxes">Generate</button>
         </div>
@@ -116,7 +50,76 @@ export default {
     </div>
   </div>
 </template>
+<script>
+import { ref } from 'vue';
+import Items from '../../assets/items.json';
 
+export default {
+  props: {
+    selected_small_number: {
+      type: Number,
+      required: true
+    }
+  },
+  data() {
+    return {
+      item: {
+        material: null,
+        name: "",
+        lore: [""],
+        selected_month: null,
+        item_number: this.selected_small_number
+      },
+      Items,
+      selected_large_number: null,
+      textBox1: "",
+      textBox2: "",
+      current_year: new Date().getFullYear(),
+      monthOptions: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+      open: false
+    };
+  },
+  computed: {
+    itemIDs() {
+      return this.Items.map(item => item.name).sort();
+    },
+  },
+
+  methods: {
+    formatItemName(name) {
+      return name.split('_')
+          .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+          .join(' ');
+    },
+    getIcon(name) {
+      return `./src/assets/items/${name}.png`;
+    },
+    addLoreField() {
+      this.item.lore.push("");
+    },
+    fillTextBoxes(){
+      const lore = this.item.lore.join('. ');
+
+      this.textBox1 = `
+  '${this.selected_small_number}'\n\
+    material: ${this.item.material.toUpperCase()}\n\
+    name: '${this.name}'\n\
+    lore:\n\
+    - '&8${lore.split('. ').join("\n    - '&8")}'\n\
+    - '&8      '\n\
+    - '&e${this.item.selected_month} ${this.current_year} Collectors Item'`;
+
+      this.textBox2 = JSON.stringify({
+        name: this.name,
+        lore: lore
+      }, null, 2);
+    },
+    toggleOpen() {
+      this.open = !this.open;
+    }
+  },
+};
+</script>
 <style scoped>
 .item-creator {
   display: flex;
